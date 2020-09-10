d63c9417cd2971ebb9c9c4b3e39980a26d3f8e0c
Blake Wang <bwang@intouchhealth.com>
2020-09-10 15:10:16 -0700

    Try non-mlrose approach

--- run.py
+++ run.py
@@ -1,97 +1,31 @@
 #!/usr/bin/env python
 
-import six
-import sys
-sys.modules['sklearn.externals.six'] = six
-import mlrose
 import numpy as np
 
-# Define a custom fitness function.
-def class_config_fitness(state, c):
-    return c*np.sum(state)
+# 0 -> 0,1
+# 2 -> 3,4,5
+# 6 -> 11,12
 
-# Set the initial state.
-# States are lists of classes sizes.
-init_state = np.array([
-    # tk_hybrid
-    0,0,0,
-    # tk_distance
-    0,0,0,
-    # tk_k_hybrid
-    0,0,0,
-    # tk_k_distance
-    0,0,0,
-    # k_hybrid
-    0,0,0,
-    # k_distance
-    0,0,0,
-    # k_1_hybrid
-    0,0,0,
-    # k_1_distance
-    0,0,0,
-    # 1_hybrid
-    0,0,0,
-    # 1_distance
-    0,0,0,
-    # 1_2_hybrid
-    0,0,0,
-    # 1_2_distance
-    0,0,0,
-    # 2_hybrid
-    0,0,0,
-    # 2_distance
-    0,0,0,
-    # 2_3_hybrid
-    0,0,0,
-    # 2_3_distance
-    0,0,0,
-    # 3_hybrid
-    0,0,0,
-    # 3_distance
-    0,0,0,
-    # 3_4_hybrid
-    0,0,0,
-    # 3_4_distance
-    0,0,0,
-    # 4_hybrid
-    0,0,0,
-    # 4_distance
-    0,0,0,
-    # 4_5_hybrid
-    0,0,0,
-    # 4_5_distance
-    0,0,0,
-    # 5_hybrid
-    0,0,0,
-    # 5_distance
-    0,0,0
-])
+GRADE_NAMES = ['tk', 'tk-k', 'k', 'k-1st', '1st', '1-2nd', '2nd', '2nd-3rd', '3rd', '3rd-4th', '4th', '4th-5th', '5th']
 
-#                tk  k   1   2   3   4   5
-dist_students = [6,  19, 30, 28, 26, 31, 24]
-hybd_students = [12, 57, 61, 68, 63, 64, 58]
-
-# Initialize the fitness function object.
-kwargs = {'c': 10}
-fitness = mlrose.CustomFitness(class_config_fitness, problem_type='discrete', **kwargs)
-print(init_state.shape[0])
-#print(fitness.evaluate(init_state))
-
-# Initialize the discrete problem object.
-problem = mlrose.DiscreteOpt(
-    length=init_state.shape[0],
-    fitness_fn=fitness,
-    maximize=False,
-    max_val=8
-)
-
+# Configuration MUST
+# - TK-3 must have average class size of 25
+# - 4,5 hard cap at 34
+# - 3/4 combo cap at 25
+# - 4/5 combo cap at 31
 #
-#best_state, best_fitness = random_hill_climb(
-#    problem,
-#    max_attempts=10,
-#    max_iters=10,
-#    restarts=0,
-#    init_state=None,
-#    curve=False,
-#    random_state=None
-#)
+# Configuration SHOULD
+# - maximize utilization
+# - minimize number of combos
+
+# Can I handle distance and hybrid totally separate? YES!
+
+#                   tk  k   1   2   3   4   5
+distance_students = [6,  19, 30, 28, 26, 31, 24]
+hybrid_students   = [12, 57, 61, 68, 63, 64, 58]
+
+def run(students):
+    state = np.zeros((len(GRADE_NAMES),3), dtype=np.int8)
+    student_grades = len(students)
+
+run(distance_students)
