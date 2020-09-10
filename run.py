e771f16c6cb758f10d8da4b2e91b355a1c73a1af
Blake Wang <bwang@intouchhealth.com>
2020-09-10 16:19:32 -0700

    Formulate new "splitting" based approach

--- run.py
+++ run.py
@@ -6,76 +6,30 @@ sys.modules['sklearn.externals.six'] = six
 import mlrose
 import numpy as np
 
+#                    t   k   1   2   3   4   5
+STUDENTS = np.array([6,  19, 30, 28, 26, 31, 24], dtype=np.int64) # distance
+#STUDENTS = np.array([12, 57, 61, 68, 63, 64, 58], dtype=np.int64) # hybrid
+
+GRADES = 'tk12345'
+STATE_LEN = (STUDENTS.shape[0] * 2 - 1) * 3
+INIT_STATE = np.linspace(0, np.sum(STUDENTS), STATE_LEN).astype(np.int64)
+print(INIT_STATE)
+exit()
+
 # Define a custom fitness function.
 def class_config_fitness(state, c):
-    return c*np.sum(state)
-
-# Set the initial state.
-# States are lists of classes sizes.
-init_state = np.array([
-    # tk_hybrid
-    0,0,0,
-    # tk_distance
-    0,0,0,
-    # tk_k_hybrid
-    0,0,0,
-    # tk_k_distance
-    0,0,0,
-    # k_hybrid
-    0,0,0,
-    # k_distance
-    0,0,0,
-    # k_1_hybrid
-    0,0,0,
-    # k_1_distance
-    0,0,0,
-    # 1_hybrid
-    0,0,0,
-    # 1_distance
-    0,0,0,
-    # 1_2_hybrid
-    0,0,0,
-    # 1_2_distance
-    0,0,0,
-    # 2_hybrid
-    0,0,0,
-    # 2_distance
-    0,0,0,
-    # 2_3_hybrid
-    0,0,0,
-    # 2_3_distance
-    0,0,0,
-    # 3_hybrid
-    0,0,0,
-    # 3_distance
-    0,0,0,
-    # 3_4_hybrid
-    0,0,0,
-    # 3_4_distance
-    0,0,0,
-    # 4_hybrid
-    0,0,0,
-    # 4_distance
-    0,0,0,
-    # 4_5_hybrid
-    0,0,0,
-    # 4_5_distance
-    0,0,0,
-    # 5_hybrid
-    0,0,0,
-    # 5_distance
-    0,0,0
-])
-
-#                tk  k   1   2   3   4   5
-dist_students = [6,  19, 30, 28, 26, 31, 24]
-hybd_students = [12, 57, 61, 68, 63, 64, 58]
+    student_str = ''
+    for i in range(len(STUDENTS)):
+        student_str += GRADES[i] * STUDENTS[i]
+    return len(student_str) * c
 
 # Initialize the fitness function object.
 kwargs = {'c': 10}
 fitness = mlrose.CustomFitness(class_config_fitness, problem_type='discrete', **kwargs)
-print(init_state.shape[0])
-#print(fitness.evaluate(init_state))
+init_state = np.zeros(STATE_LEN, dtype=np.int64)
+print(STATE_LEN)
+print(fitness.evaluate(init_state))
+exit()
 
 # Initialize the discrete problem object.
 problem = mlrose.DiscreteOpt(
