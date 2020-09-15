f4458f35362e22931a03b7e01149a0e6ef7f38e8
Blake Wang <bwang@intouchhealth.com>
2020-09-15 16:01:11 -0700

    Start using mlrose

--- run.py
+++ run.py
@@ -5,47 +5,64 @@ import sys
 sys.modules['sklearn.externals.six'] = six
 import mlrose
 import numpy as np
+import itertools
 
-#                    t   k   1   2   3   4   5
-STUDENTS = np.array([6,  19, 30, 28, 26, 31, 24], dtype=np.int64) # distance
-#STUDENTS = np.array([12, 57, 61, 68, 63, 64, 58], dtype=np.int64) # hybrid
+# Make experiments repeatable.
+RANDOM_STATE = 255
 
-GRADES = 'tk12345'
-STATE_LEN = (STUDENTS.shape[0] * 2 - 1) * 3
-INIT_STATE = np.linspace(0, np.sum(STUDENTS), STATE_LEN).astype(np.int64)
-print(INIT_STATE)
-exit()
+# Set how many students are in each grade.
+STUDENT_INPUT = np.array([6,  19, 30, 28, 26, 31, 24], dtype=np.int64) # distance
+#STUDENT_INPUT = np.array([12, 57, 61, 68, 63, 64, 58], dtype=np.int64) # hybrid
+
+# Calculate the length of the state array.
+STATE_LEN = STUDENT_INPUT.shape[0] * 3
+#STATE_LEN = (STUDENT_INPUT.shape[0] * 2 - 1) * 3
+
+# Populate a list of students, represented by their grade level.
+STUDENTS = []
+for i in range(len(STUDENT_INPUT)):
+    for j in range(STUDENT_INPUT[i]):
+        STUDENTS.append(i)
+STUDENTS = np.array(STUDENTS, dtype=np.int64)
+
+# Build the initial state by dividing the STUDENTS array evenly.
+# Each state is an array of indices for splitting the STUDENTS array.
+INIT_STATE = np.linspace(0, np.sum(STUDENT_INPUT) - 1, STATE_LEN).astype(np.int64)
 
 # Define a custom fitness function.
 def class_config_fitness(state, c):
-    student_str = ''
-    for i in range(len(STUDENTS)):
-        student_str += GRADES[i] * STUDENTS[i]
-    return len(student_str) * c
+    fitness = 0
+#    state = np.sort(state)
+    classes = np.array(np.split(STUDENTS, state))
+
+    for klass in classes:
+        fitness -= klass.shape[0]
+
+    return fitness
 
 # Initialize the fitness function object.
 kwargs = {'c': 10}
 fitness = mlrose.CustomFitness(class_config_fitness, problem_type='discrete', **kwargs)
-init_state = np.zeros(STATE_LEN, dtype=np.int64)
-print(STATE_LEN)
-print(fitness.evaluate(init_state))
-exit()
 
 # Initialize the discrete problem object.
 problem = mlrose.DiscreteOpt(
-    length=init_state.shape[0],
+    length=INIT_STATE.shape[0],
     fitness_fn=fitness,
-    maximize=False,
-    max_val=8
+    maximize=True,
+    max_val=STUDENTS.shape[0]
 )
 
-#
-#best_state, best_fitness = random_hill_climb(
-#    problem,
-#    max_attempts=10,
-#    max_iters=10,
-#    restarts=0,
-#    init_state=None,
-#    curve=False,
-#    random_state=None
-#)
+best_state, best_fitness = mlrose.random_hill_climb(
+    problem,
+    max_attempts=10,
+    max_iters=1000,
+    restarts=100,
+    init_state=INIT_STATE,
+    curve=False,
+    random_state=RANDOM_STATE
+)
+
+print('INIT_STATE', INIT_STATE)
+print('INIT_STATE fitness', fitness.evaluate(INIT_STATE))
+print('best_state', best_state)
+print('best_state fitness', fitness.evaluate(best_state))
