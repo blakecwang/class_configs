6f41527036d0ac5bc4b216beb1ccb20ead620a9d
Blake Wang <bwang@intouchhealth.com>
2020-09-16 10:57:50 -0700

    Use exponential reward to get close to target

--- run.py
+++ run.py
@@ -11,11 +11,11 @@ import itertools
 RANDOM_STATE = 255
 
 # Set how many students are in each grade.
-STUDENT_INPUT = np.array([6,  19, 30, 28, 26, 31, 24], dtype=np.int64) # distance
+STUDENT_INPUT = np.array([6, 19, 30, 28, 26, 31, 24], dtype=np.int64) # distance
 #STUDENT_INPUT = np.array([12, 57, 61, 68, 63, 64, 58], dtype=np.int64) # hybrid
 
 # Calculate the length of the state array.
-STATE_LEN = STUDENT_INPUT.shape[0] * 3
+STATE_LEN = 4
 #STATE_LEN = (STUDENT_INPUT.shape[0] * 2 - 1) * 3
 
 # Populate a list of students, represented by their grade level.
@@ -30,19 +30,20 @@ STUDENTS = np.array(STUDENTS, dtype=np.int64)
 INIT_STATE = np.linspace(0, np.sum(STUDENT_INPUT) - 1, STATE_LEN).astype(np.int64)
 
 # Define a custom fitness function.
-def class_config_fitness(state, c):
+def klass_config_fitness(state, c):
     fitness = 0
-#    state = np.sort(state)
-    classes = np.array(np.split(STUDENTS, state))
+    state = np.sort(state)
+    klasses = np.array(np.split(STUDENTS, state))
 
-    for klass in classes:
-        fitness -= klass.shape[0]
+    for klass in klasses:
+        klass_size = klass.shape[0]
+        fitness -= (31 - klass_size) ** 2
 
     return fitness
 
 # Initialize the fitness function object.
 kwargs = {'c': 10}
-fitness = mlrose.CustomFitness(class_config_fitness, problem_type='discrete', **kwargs)
+fitness = mlrose.CustomFitness(klass_config_fitness, problem_type='discrete', **kwargs)
 
 # Initialize the discrete problem object.
 problem = mlrose.DiscreteOpt(
@@ -54,7 +55,7 @@ problem = mlrose.DiscreteOpt(
 
 best_state, best_fitness = mlrose.random_hill_climb(
     problem,
-    max_attempts=10,
+    max_attempts=100,
     max_iters=1000,
     restarts=100,
     init_state=INIT_STATE,
@@ -64,5 +65,11 @@ best_state, best_fitness = mlrose.random_hill_climb(
 
 print('INIT_STATE', INIT_STATE)
 print('INIT_STATE fitness', fitness.evaluate(INIT_STATE))
+
+print('--')
 print('best_state', best_state)
 print('best_state fitness', fitness.evaluate(best_state))
+
+klasses = np.array(np.split(STUDENTS, np.sort(best_state)))
+for klass in klasses:
+    print(klass.shape[0])
